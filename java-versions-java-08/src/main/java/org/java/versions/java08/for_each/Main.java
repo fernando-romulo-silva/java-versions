95ec6c8d35d01ffcbfad4d5a1b36d4022fbbcdd4
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2024-11-18 07:58:11 -0300

    news

--- java-versions-java-08/src/main/java/org/java/versions/java08/for_each/Main.java
+++ java-versions-java-08/src/main/java/org/java/versions/java08/for_each/Main.java
@@ -1,23 +1,25 @@
 package org.java.versions.java08.for_each;
 
+import static java.lang.System.out;
+
 import java.util.ArrayList;
 import java.util.List;
 
 class Main {
 
 	public static void main(String[] args) {
-		List<String> gamesList = new ArrayList<String>();
+		List<String> gamesList = new ArrayList<>();
 
 		gamesList.add("Football");
 		gamesList.add("Cricket");
 		gamesList.add("Chess");
 		gamesList.add("Hocky");
 
-		System.out.println("------------Iterating by passing lambda expression--------------");
-		gamesList.forEach(games -> System.out.println(games));
+		out.println("------------Iterating by passing lambda expression--------------");
+		gamesList.forEach(games -> out.println(games));
 
-		System.out.println("------------Iterating by passing method reference---------------");
-		gamesList.forEach(System.out::println);
+		out.println("------------Iterating by passing method reference---------------");
+		gamesList.forEach(out::println);
 
 	}
 
